31bd405b43cc8eecb04b1faa57f0f68781e34a03
Philip Daniels <Philip.Daniels1971@gmail.com>
2014-07-28 22:16:32 +0100

    Introduce NUnit.

---
+++ Lithogen.Core.Tests/BuildContextTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Method_StateUnderTest_ExpectedBehavior.
+// It is nice to Assert.NotNull before checking more specific criteria as it results in a more meaningful error message.
+
+namespace Lithogen.Core.Tests
+{
+    [TestFixture]
+    public class BuildContextTests
+    {
+        [Test]
+        public void Ctor_Executed_CreatesEmptyTagDataDictionary()
+        {
+            var bc = new BuildContext();
+            Assert.NotNull(bc.TagData);
+            Assert.IsEmpty(bc.TagData);
+        }
+    }
+}
