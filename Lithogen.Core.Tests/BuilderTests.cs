00b1a219becc215722e9c12ddc8424675e27cdf5
Philip Daniels <Philip.Daniels1971@gmail.com>
2014-07-29 21:28:52 +0100

    Introduce Moq and use it for just 1 mock object in BuilderTests.cs.

---
+++ Lithogen.Core.Tests/BuilderTests.cs
@@ -0,0 +1,31 @@
+﻿using Lithogen.Interfaces;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lithogen.Core.Tests
+{
+    [TestFixture]
+    public class BuilderTests
+    {
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Ctor_WhenBuildContextIsNull_ThrowsArgumentNullException()
+        {
+            var b = new Builder(null);
+        }
+
+        [Test]
+        public void Ctor_Executed_CreatesEmptyStepsCollection()
+        {
+            var bc = new Mock<IBuildContext>();
+            var b = new Builder(bc.Object);
+            Assert.NotNull(b.Steps);
+            Assert.IsEmpty(b.Steps);
+        }
+    }
+}
